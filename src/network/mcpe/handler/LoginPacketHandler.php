0506f5c5646dd52bedcf9e809e5ea9e4567e627e
aabstractt <xavier04.martinez@gmail.com>
2022-09-22 14:06:45 -0500

    fix: fix protocol version

--- src/network/mcpe/handler/LoginPacketHandler.php
+++ src/network/mcpe/handler/LoginPacketHandler.php
@@ -238,6 +238,6 @@ class LoginPacketHandler extends PacketHandler{
 	}
 
 	protected function isCompatibleProtocol(int $protocolVersion) : bool{
-		return in_array($protocolVersion, [ProtocolInfo::CURRENT_PROTOCOL, 545, 551, 552, 554], true);
+		return $protocolVersion === ProtocolInfo::CURRENT_PROTOCOL;
 	}
 }
