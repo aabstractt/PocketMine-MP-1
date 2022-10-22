f8b66953b2b240a2055d2db07f22ec6f3ecf5dc1
aabstractt <xavier04.martinez@gmail.com>
2022-10-22 17:09:00 -0500

    feat: small change

--- src/command/defaults/SetViewDistanceCommand.php
+++ src/command/defaults/SetViewDistanceCommand.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace pocketmine\command\defaults;
 
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
 use pocketmine\Server;
 use pocketmine\utils\TextFormat;
 
@@ -31,6 +32,8 @@ final class SetViewDistanceCommand extends VanillaCommand {
 		Server::getInstance()->getConfigGroup()->setConfigInt("view-distance", intval($args[0]));
 		Server::getInstance()->getConfigGroup()->save();
 
-		$sender->sendMessage(TextFormat::GREEN . 'The server view distance was changed to ' . $args[0]);
+		foreach (Server::getInstance()->getOnlinePlayers() as $target) $target->setViewDistance(intval($args[0]));
+
+		Server::getInstance()->broadcastMessage(TextFormat::GREEN . 'The server view distance was changed to ' . $args[0]);
 	}
 }
